a4fb38d3d93a0a2c2c434a89624ce40355104646
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-03-15 22:38:43 +1030

    feat(core): explosed app component via application class

--- app-components/src/main/java/org/synsystems/onlypass/components/App.java
+++ app-components/src/main/java/org/synsystems/onlypass/components/App.java
@@ -3,8 +3,16 @@ package org.synsystems.onlypass.components;
 import android.app.Application;
 
 public class App extends Application {
+  private AppComponent appComponent;
+
   @Override
   public void onCreate() {
     super.onCreate();
+
+    appComponent = DaggerAppComponent.create();
+  }
+
+  public AppComponent getAppComponent() {
+    return appComponent;
   }
 }
