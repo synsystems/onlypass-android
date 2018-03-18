a3d825568577e9f0328f79d197c436ea0aeb65f7
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-03-18 20:36:07 +1030

    feat(events): changed events interface and updated subclasses to match

--- framework/src/main/java/org/synsystems/onlypass/framework/events/Event.java
+++ framework/src/main/java/org/synsystems/onlypass/framework/events/Event.java
@@ -3,28 +3,41 @@ package org.synsystems.onlypass.framework.events;
 import android.support.annotation.NonNull;
 
 import org.joda.time.LocalDateTime;
-import org.synsystems.onlypass.framework.rxutils.Pulse;
 
 import io.reactivex.Completable;
-import io.reactivex.Maybe;
 import io.reactivex.Observable;
 
 /**
  * Persistent record of an event that can happen during app execution.
  */
 public interface Event {
+  /**
+   * Creates an observable that emits each time a new occurrence is declared. The emitted times are not guaranteed to
+   * actually be in the past, since {@link #declareOccurredAt(LocalDateTime)} does not perform validation.
+   *
+   * @return the new observable
+   */
   @NonNull
-  public Observable<Pulse> observeOccurrences();
+  public Observable<LocalDateTime> observeNewOccurrences();
 
+  /**
+   * Creates an observable the occurrences that have been previously declared and then completes. The emitted times are
+   * not guaranteed to actually be in the past, since {@link #declareOccurredAt(LocalDateTime)} does not perform
+   * validation.
+   *
+   * @return the new observable
+   */
   @NonNull
-  public Maybe<LocalDateTime> getLatestOccurrence();
+  public Observable<LocalDateTime> observePastOccurrences();
 
-  @NonNull
-  public Observable<LocalDateTime> getAllOccurrences();
-
-  @NonNull
-  public Completable declareOccurredNow();
-
+  /**
+   * Records the supplied time as an occurrence of this event. All times are valid, including times in the future.
+   *
+   * @param time
+   *     the time to record
+   *
+   * @return a completable that performs the operation
+   */
   @NonNull
   public Completable declareOccurredAt(@NonNull LocalDateTime time);
 }
--- framework/src/main/java/org/synsystems/onlypass/framework/events/SharedPreferencesBackedEvent.java
+++ framework/src/main/java/org/synsystems/onlypass/framework/events/SharedPreferencesBackedEvent.java
@@ -3,70 +3,55 @@ package org.synsystems.onlypass.framework.events;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 
-import com.google.common.base.Supplier;
-
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.LocalDateTime;
-import org.synsystems.onlypass.framework.rxutils.Pulse;
 
 import java.util.HashSet;
 
 import io.reactivex.Completable;
-import io.reactivex.Maybe;
 import io.reactivex.Observable;
 import io.reactivex.Single;
 import io.reactivex.subjects.PublishSubject;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+/**
+ * An event that uses shared preferences to store the occurrence history.
+ */
 public class SharedPreferencesBackedEvent implements Event {
-  private final PublishSubject<Pulse> occurrences = PublishSubject.create();
+  private final PublishSubject<LocalDateTime> newOccurrences = PublishSubject.create();
 
   private final SharedPreferences sharedPrefs;
 
   private final String prefKey;
 
-  private final Supplier<LocalDateTime> nowSupplier;
-
-  public SharedPreferencesBackedEvent(
-      @NonNull final SharedPreferences sharedPrefs,
-      @NonNull final String prefKey,
-      @NonNull final Supplier<LocalDateTime> nowSupplier) {
-
+  /**
+   * Constructs a new SharedPreferencesBackedEvent.
+   *
+   * @param sharedPrefs
+   *     the preferences to store the occurrence history in
+   * @param prefKey
+   *     the key to store the occurrence history with
+   */
+  public SharedPreferencesBackedEvent(@NonNull final SharedPreferences sharedPrefs, @NonNull final String prefKey) {
     this.sharedPrefs = checkNotNull(sharedPrefs);
     this.prefKey = checkNotNull(prefKey);
-    this.nowSupplier = checkNotNull(nowSupplier);
   }
 
   @NonNull
   @Override
-  public Observable<Pulse> observeOccurrences() {
-    return occurrences;
+  public Observable<LocalDateTime> observeNewOccurrences() {
+    return newOccurrences.distinct();
   }
 
   @NonNull
   @Override
-  public Maybe<LocalDateTime> getLatestOccurrence() {
-    return getAllOccurrences()
-        .sorted(LocalDateTime::compareTo)
-        .lastElement();
-  }
-
-  @NonNull
-  @Override
-  public Observable<LocalDateTime> getAllOccurrences() {
+  public Observable<LocalDateTime> observePastOccurrences() {
     return Single
         .fromCallable(() -> sharedPrefs.getStringSet(prefKey, new HashSet<>()))
         .flatMapObservable(Observable::fromIterable)
-        .distinct()
         .flatMapSingle(this::deserialiseLocalDateTime);
-  }
-
-  @NonNull
-  @Override
-  public Completable declareOccurredNow() {
-    return declareOccurredAt(nowSupplier.get());
   }
 
   @NonNull
@@ -74,15 +59,16 @@ public class SharedPreferencesBackedEvent implements Event {
   public Completable declareOccurredAt(@NonNull final LocalDateTime time) {
     checkNotNull(time);
 
-    return getAllOccurrences()
+    return observePastOccurrences()
         .concatWith(Observable.just(time))
+        .distinct()
         .flatMapSingle(this::serialiseLocalDateTime)
         .collectInto(new HashSet<String>(), HashSet::add)
         .flatMapCompletable(set -> Completable.fromRunnable(() -> sharedPrefs
             .edit()
             .putStringSet(prefKey, set)
             .apply()))
-        .andThen(Completable.fromRunnable(() -> occurrences.onNext(Pulse.getInstance())));
+        .andThen(Completable.fromRunnable(() -> newOccurrences.onNext(time)));
   }
 
   private Single<String> serialiseLocalDateTime(@NonNull final LocalDateTime date) {
