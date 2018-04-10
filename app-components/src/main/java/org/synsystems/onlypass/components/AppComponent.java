f5bad748a3ac76e82ecccf70047a83e127d9bd2c
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-04-10 21:42:57 +0930

    feat(core): added cryptography modules to app component

--- app-components/src/main/java/org/synsystems/onlypass/components/AppComponent.java
+++ app-components/src/main/java/org/synsystems/onlypass/components/AppComponent.java
@@ -1,11 +1,22 @@
 package org.synsystems.onlypass.components;
 
+import org.synsystems.onlypass.components.cryptography.credentialhardening.PasswordBasedKeyDerivationConfigurationModule;
+import org.synsystems.onlypass.components.cryptography.credentialhardening.PasswordBasedKeyDerivationCredentialHardenerModule;
+import org.synsystems.onlypass.components.cryptography.encryption.aes.AesProviderModule;
+import org.synsystems.onlypass.components.cryptography.encryption.aes.DerivedKeyCredentialConverterModule;
 import org.synsystems.onlypass.components.logging.LoggingModule;
 import org.synsystems.onlypass.components.preferences.PreferencesModule;
 
 import dagger.Component;
 
-@Component(modules = {AppModule.class, LoggingModule.class, PreferencesModule.class})
+@Component(modules = {
+    AppModule.class,
+    LoggingModule.class,
+    PreferencesModule.class,
+    PasswordBasedKeyDerivationConfigurationModule.class,
+    PasswordBasedKeyDerivationCredentialHardenerModule.class,
+    AesProviderModule.class,
+    DerivedKeyCredentialConverterModule.class})
 @AppScope
 public interface AppComponent {
   public Environment getEnvironment();
@@ -14,11 +25,21 @@ public interface AppComponent {
 
   @Component.Builder
   public interface Builder {
-    public Builder setAppModule(AppModule appModule);
+    public Builder setAppModule(AppModule module);
 
-    public Builder setLoggingModule(LoggingModule loggingModule);
+    public Builder setLoggingModule(LoggingModule module);
 
-    public Builder setPreferencesModule(PreferencesModule preferencesModule);
+    public Builder setPreferencesModule(PreferencesModule module);
+
+    public Builder setPasswordBasedKeyDerivationConfigurationModule(
+        PasswordBasedKeyDerivationConfigurationModule module);
+
+    public Builder setPasswordBasedKeyDerivationCredentialHardenerModule(
+        PasswordBasedKeyDerivationCredentialHardenerModule module);
+
+    public Builder setAesProviderModule(AesProviderModule module);
+
+    public Builder setDerivedKeyCredentialConverterModule(DerivedKeyCredentialConverterModule module);
 
     public AppComponent build();
   }
