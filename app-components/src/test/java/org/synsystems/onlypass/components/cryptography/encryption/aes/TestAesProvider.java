51f977220f76881d00ed37d0eec5e06a0528141c
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-04-12 18:14:05 +0930

    test(aes): updated tests to use new hardening parameters class

--- app-components/src/test/java/org/synsystems/onlypass/components/cryptography/encryption/aes/TestAesProvider.java
+++ app-components/src/test/java/org/synsystems/onlypass/components/cryptography/encryption/aes/TestAesProvider.java
@@ -3,9 +3,8 @@ package org.synsystems.onlypass.components.cryptography.encryption.aes;
 import org.junit.Before;
 import org.junit.Test;
 import org.synsystems.onlypass.components.cryptography.credentialhardening.CredentialHardener;
-import org.synsystems.onlypass.components.cryptography.credentialhardening.HashingSalt;
-import org.synsystems.onlypass.components.cryptography.credentialhardening.PasswordBasedKeyDerivationConfiguration;
-import org.synsystems.onlypass.components.cryptography.credentialhardening.PasswordBasedKeyDerivationCredentialHardener;
+import org.synsystems.onlypass.components.cryptography.credentialhardening.Pbkdf2WithHmacSha256CredentialHardener;
+import org.synsystems.onlypass.components.cryptography.credentialhardening.Pbkdf2WithHmacSha256Parameters;
 import org.synsystems.onlypass.components.cryptography.credentials.CleartextPassword;
 import org.synsystems.onlypass.components.cryptography.credentials.DerivedKey;
 import org.synsystems.onlypass.components.cryptography.encryption.EncryptionError;
@@ -17,11 +16,14 @@ import java.util.Arrays;
 import javax.crypto.NoSuchPaddingException;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
-import static org.mockito.Mockito.mock;
 
 @SuppressWarnings({"ConstantConditions", "unchecked"})
 public class TestAesProvider {
   private AesProvider<DerivedKey> aesProvider;
+
+  private byte[] iv1;
+
+  private byte[] iv2;
 
   private DerivedKey key1;
 
@@ -29,80 +31,74 @@ public class TestAesProvider {
 
   @Before
   public void setup() throws NoSuchPaddingException, NoSuchAlgorithmException {
-    final AesConfiguration aesConfiguration = AesConfiguration
-        .builder()
-        .setInitialisationVectorBitlength(128)
-        .build();
-
     final CredentialConverter<DerivedKey> credentialConverter = new DerivedKeyCredentialConverter();
 
-    aesProvider = new AesProvider<>(aesConfiguration, credentialConverter);
+    aesProvider = new AesProvider<>(credentialConverter);
 
-    final CredentialHardener<CleartextPassword, DerivedKey, HashingSalt> credentialHardener = new
-        PasswordBasedKeyDerivationCredentialHardener(
-        PasswordBasedKeyDerivationConfiguration
-            .builder()
-            .setDerivedKeyBitlength(256)
-            .setIterationCount(100)
-            .build());
+    iv1 = new byte[128];
+    iv2 = new byte[96];
+
+    final CredentialHardener<CleartextPassword, DerivedKey, Pbkdf2WithHmacSha256Parameters> credentialHardener =
+        new Pbkdf2WithHmacSha256CredentialHardener();
+
+    final Pbkdf2WithHmacSha256Parameters hardeningParameters = Pbkdf2WithHmacSha256Parameters
+        .builder()
+        .setSalt(new byte[]{1})
+        .setDerivedKeyBitlength(256)
+        .setIterationCount(1000)
+        .build();
 
     key1 = credentialHardener
-        .hardenCredential(CleartextPassword.create("password"), HashingSalt.create(new byte[]{0, 1, 2, 3, 4}))
+        .hardenCredential(CleartextPassword.create("password"), hardeningParameters)
         .blockingGet();
 
     key2 = credentialHardener
-        .hardenCredential(CleartextPassword.create("not password"), HashingSalt.create(new byte[]{0, 1, 2, 3, 4}))
+        .hardenCredential(CleartextPassword.create("not password"), hardeningParameters)
         .blockingGet();
-  }
-
-  @Test(expected = RuntimeException.class)
-  public void testConstructor_nullAesConfiguration() throws NoSuchPaddingException, NoSuchAlgorithmException {
-    new AesProvider(null, mock(CredentialConverter.class));
   }
 
   @Test(expected = RuntimeException.class)
   public void testConstructor_nullCredentialConverter() throws NoSuchPaddingException, NoSuchAlgorithmException {
-    new AesProvider(mock(AesConfiguration.class), null);
+    new AesProvider(null);
   }
 
   @Test(expected = RuntimeException.class)
   public void testEncrypt_nullCleartext() {
-    aesProvider.encrypt(null, new byte[0], mock(DerivedKey.class));
+    aesProvider.encrypt(null, iv1, key1);
   }
 
   @Test(expected = RuntimeException.class)
   public void testEncrypt_nullInitialisationVector() {
-    aesProvider.encrypt(new byte[0], null, mock(DerivedKey.class));
+    aesProvider.encrypt(new byte[0], null, key1);
   }
 
   @Test(expected = RuntimeException.class)
   public void testEncrypt_nullCredential() {
-    aesProvider.encrypt(new byte[0], new byte[0], null);
+    aesProvider.encrypt(new byte[0], iv1, null);
   }
 
   @Test(expected = RuntimeException.class)
   public void testDecrypt_nullCiphertext() {
-    aesProvider.decrypt(null, new byte[0], mock(DerivedKey.class));
+    aesProvider.decrypt(null, iv1, key1);
   }
 
   @Test(expected = RuntimeException.class)
   public void testDecrypt_nullInitialisationVector() {
-    aesProvider.decrypt(new byte[0], null, mock(DerivedKey.class));
+    aesProvider.decrypt(new byte[0], null, key1);
   }
 
   @Test(expected = RuntimeException.class)
   public void testDecrypt_nullCredential() {
-    aesProvider.decrypt(new byte[0], new byte[0], null);
+    aesProvider.decrypt(new byte[0], iv1, null);
   }
 
   @Test
   public void testEncryptThenDecrypt_emptyCiphertext() {
     final byte[] cleartext = new byte[0];
-    final byte[] iv = new byte[]{0, 1, 2, 3, 4, 5, 6, 7};
 
     aesProvider
-        .encrypt(cleartext, iv, key1)
-        .flatMap(ciphertext -> aesProvider.decrypt(ciphertext, iv, key1))
+        .encrypt(cleartext, iv1, key1)
+        .flatMap(ciphertext -> aesProvider.decrypt(ciphertext, iv1, key1))
         .test()
         .awaitDone(200, MILLISECONDS)
         .assertNoErrors()
@@ -120,16 +116,14 @@ public class TestAesProvider {
         .flatMap(ciphertext -> aesProvider.decrypt(ciphertext, iv, key1))
         .test()
         .awaitDone(200, MILLISECONDS)
-        .assertNoErrors()
-        .assertValue(decryptedData -> Arrays.equals(decryptedData, cleartext))
-        .assertComplete();
+        .assertError(EncryptionError.class)
+        .assertNoValues()
+        .assertNotComplete();
   }
 
   @Test
   public void testEncryptThenDecrypt_initialisationVectorsDoNotMatch() {
     final byte[] cleartext = new byte[]{0, 1, 2, 3, 4, 5, 6, 7};
-    final byte[] iv1 = new byte[]{8, 9, 10, 11, 12, 13, 14, 15};
-    final byte[] iv2 = new byte[]{16, 17, 18, 19, 20, 21, 22, 23};
 
     aesProvider
         .encrypt(cleartext, iv1, key1)
@@ -144,11 +138,10 @@ public class TestAesProvider {
   @Test
   public void testEncryptThenDecrypt_credentialsDoNotMatch() {
     final byte[] cleartext = new byte[]{0, 1, 2, 3, 4, 5, 6, 7};
-    final byte[] iv = new byte[]{8, 9, 10, 11, 12, 13, 14, 15};
 
     aesProvider
-        .encrypt(cleartext, iv, key1)
-        .flatMap(ciphertext -> aesProvider.decrypt(ciphertext, iv, key2))
+        .encrypt(cleartext, iv1, key1)
+        .flatMap(ciphertext -> aesProvider.decrypt(ciphertext, iv1, key2))
         .test()
         .awaitDone(200, MILLISECONDS)
         .assertError(EncryptionError.class)
@@ -157,13 +150,12 @@ public class TestAesProvider {
   }
 
   @Test
-  public void testEncryptThenDecrypt_initialisationVectorAndCredentialsMatch() {
+  public void testEncryptThenDecrypt_initialisationVectorsAndCredentialsMatch() {
     final byte[] cleartext = new byte[]{0, 1, 2, 3, 4, 5, 6, 7};
-    final byte[] iv = new byte[]{8, 9, 10, 11, 12, 13, 14, 15};
 
     aesProvider
-        .encrypt(cleartext, iv, key1)
-        .flatMap(ciphertext -> aesProvider.decrypt(ciphertext, iv, key1))
+        .encrypt(cleartext, iv1, key1)
+        .flatMap(ciphertext -> aesProvider.decrypt(ciphertext, iv1, key1))
         .test()
         .awaitDone(200, MILLISECONDS)
         .assertNoErrors()
