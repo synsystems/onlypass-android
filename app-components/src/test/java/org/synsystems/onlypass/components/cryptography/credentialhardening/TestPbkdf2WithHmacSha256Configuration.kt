8b8f81ee3eb31f310248030f382c71bdec63db4b
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-09-06 11:24:34 -0700

    test(credentialhardening): added edge case tests

--- app-components/src/test/java/org/synsystems/onlypass/components/cryptography/credentialhardening/TestPbkdf2WithHmacSha256Configuration.kt
+++ app-components/src/test/java/org/synsystems/onlypass/components/cryptography/credentialhardening/TestPbkdf2WithHmacSha256Configuration.kt
@@ -6,11 +6,17 @@ class TestPbkdf2WithHmacSha256Configuration {
   @Test(expected = RuntimeException::class)
   fun testBuild_emptySaltSet() = Pbkdf2WithHmacSha256Configuration(arrayOf(), 1, 8)
 
+  @Test
+  fun testBuild_nonEmptySaltSet() = Pbkdf2WithHmacSha256Configuration(arrayOf(1), 1, 8)
+
   @Test(expected = RuntimeException::class)
   fun testBuild_iterationCountLessThan1() = Pbkdf2WithHmacSha256Configuration(arrayOf(1), 0, 8)
 
   @Test
   fun testBuild_iterationCountEqualTo1() = Pbkdf2WithHmacSha256Configuration(arrayOf(1), 1, 8)
+
+  @Test
+  fun testBuild_iterationCountGreaterThan1() = Pbkdf2WithHmacSha256Configuration(arrayOf(1), 2, 8)
 
   @Test(expected = RuntimeException::class)
   fun testBuild_derivedKeyBitlengthLessThan8() = Pbkdf2WithHmacSha256Configuration(arrayOf(1), 1, 7)
