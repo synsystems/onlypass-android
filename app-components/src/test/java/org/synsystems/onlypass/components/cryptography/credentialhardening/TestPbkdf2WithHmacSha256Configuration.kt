857b94ece520d697e49ac792aa1635f2b8c7fbce
Matthew Tamlin <matthew.tamlin@icloud.com>
2018-09-06 11:18:42 -0700

    test(credentialhardening): converted Java classes to Kotlin

---
+++ app-components/src/test/java/org/synsystems/onlypass/components/cryptography/credentialhardening/TestPbkdf2WithHmacSha256Configuration.kt
@@ -0,0 +1,26 @@
+package org.synsystems.onlypass.components.cryptography.credentialhardening
+
+import org.junit.Test
+
+class TestPbkdf2WithHmacSha256Configuration {
+  @Test(expected = RuntimeException::class)
+  fun testBuild_emptySaltSet() = Pbkdf2WithHmacSha256Configuration(arrayOf(), 1, 8)
+
+  @Test(expected = RuntimeException::class)
+  fun testBuild_iterationCountLessThan1() = Pbkdf2WithHmacSha256Configuration(arrayOf(1), 0, 8)
+
+  @Test
+  fun testBuild_iterationCountEqualTo1() = Pbkdf2WithHmacSha256Configuration(arrayOf(1), 1, 8)
+
+  @Test(expected = RuntimeException::class)
+  fun testBuild_derivedKeyBitlengthLessThan8() = Pbkdf2WithHmacSha256Configuration(arrayOf(1), 1, 7)
+
+  @Test
+  fun testBuild_derivedKeyBitlengthEqualTo8() = Pbkdf2WithHmacSha256Configuration(arrayOf(1), 1, 8)
+
+  @Test
+  fun testBuild_derivedKeyBitlengthGreaterThan8() = Pbkdf2WithHmacSha256Configuration(arrayOf(1), 1, 16)
+
+  @Test(expected = RuntimeException::class)
+  fun testBuild_derivedKeyBitlengthNotAMultipleOf8() = Pbkdf2WithHmacSha256Configuration(arrayOf(1), 1, 9)
+}
